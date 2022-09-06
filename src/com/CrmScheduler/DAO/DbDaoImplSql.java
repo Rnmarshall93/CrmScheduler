78e5e7178e99e282ee56fcfb5aed7959e369c814
Rnmarshall <rnmarshall1993@gmail.com>
2022-09-06 12:01:54 -0700

    Implemented DbDaoImplSql

--- src/com/CrmScheduler/DAO/DbDaoImplSql.java
+++ src/com/CrmScheduler/DAO/DbDaoImplSql.java
@@ -1,1 +1,49 @@
-package com.CrmScheduler.DAO;  public class DbDaoImplSql { }
+package com.CrmScheduler.DAO;
+
+import com.CrmScheduler.HibernateConf;
+import com.CrmScheduler.entity.Country;
+import com.CrmScheduler.entity.DbLog;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+
+public class DbDaoImplSql implements IDbDao{
+    @Override
+    public void createdDbLog(DbLog dbLog) {
+        Session session = HibernateConf.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        session.save(dbLog);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public DbLog getDbLog(long dbLogId) {
+        Session session = HibernateConf.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        DbLog foundDbLog = session.get(DbLog.class, dbLogId);
+
+        session.getTransaction().commit();
+        session.close();
+
+        return foundDbLog;
+    }
+
+    @Override
+    public ArrayList<DbLog> getAllDbLogs() {
+        Session session = HibernateConf.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        ArrayList<DbLog> allDbLogs = new ArrayList<>();
+        Query query = session.createQuery("from DbLog");
+        allDbLogs.addAll(query.list());
+
+        session.getTransaction().commit();
+        session.close();
+        return allDbLogs;
+    }
+}
